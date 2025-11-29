7bd4d75b631e1e9c1d8b808a400be74a2f140b35
KattyAlice <kattyalice@yahoo.com>
2025-11-29 07:49:46 -0500

    updated swagger

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 from app import create_app
 from app.models import db
 from config import ProductionConfig
+from flask import redirect
 
 app = create_app("ProductionConfig")
+
+@app.route("/", methods=['GET'])
+def index():
+    return redirect('/api/docs')
 
 with app.app_context():
     db.create_all()
