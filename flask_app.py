33a8e8394ba4e36d1de26fe08a5f96be60c408d9
KattyAlice <kattyalice@yahoo.com>
2025-12-03 13:58:26 -0500

    project requirement updates, flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,9 @@
 from app import create_app
-from app.models import db
 from config import ProductionConfig
 from flask import redirect
 
 app = create_app("ProductionConfig")
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    return redirect('/api/docs')
-
-with app.app_context():
-    db.create_all()
+    return redirect("/api/docs")
